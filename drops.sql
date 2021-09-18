0a28184401a94f650f0f508de5647a83e125bbf5
Dungeon Master <dungeon@master.com>
2021-09-18 20:07:49 +0300

    fix: insertions.sql

--- drops.sql
+++ drops.sql
@@ -1,49 +1,49 @@
 -- owners table
-DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS owners CASCADE;
 
 -- realties for owners table
-DROP TABLE IF EXISTS realties;
+DROP TABLE IF EXISTS realties CASCADE;
 
 -- ownerships facts for owners table
-DROP TABLE IF EXISTS ownerships;
+DROP TABLE IF EXISTS ownerships CASCADE;
 
 -- brains of looking_at_the_moon table
-DROP TABLE IF EXISTS brains;
+DROP TABLE IF EXISTS brains CASCADE;
 
 -- looking_at_the_moon table
-DROP TABLE IF EXISTS looking_at_the_moon;
+DROP TABLE IF EXISTS looking_at_the_moon CASCADE;
 
 -- states for impulses table
-DROP TABLE IF EXISTS states;
+DROP TABLE IF EXISTS states CASCADE;
 
 -- impulses to characterize looking_at_the_moon table
-DROP TABLE IF EXISTS impulses;
+DROP TABLE IF EXISTS impulses CASCADE;
 
 -- properties for impulses table
-DROP TABLE IF EXISTS properties;
+DROP TABLE IF EXISTS properties CASCADE;
 
 -- materials for monoliths
-DROP TABLE IF EXISTS materials;
+DROP TABLE IF EXISTS materials CASCADE;
 
 -- monoliths table
-DROP TABLE IF EXISTS monoliths;
+DROP TABLE IF EXISTS monoliths CASCADE;
 
 -- dreams of monoliths and brains table
-DROP TABLE IF EXISTS dreams;
+DROP TABLE IF EXISTS dreams CASCADE;
 
 -- images of dreams table
-DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS images CASCADE;
 
 -- aliens of realties table
 -- not the Ridley Scott aliens uWu
-DROP TABLE IF EXISTS aliens;
+DROP TABLE IF EXISTS aliens CASCADE;
 
 
 -- expulsions of aliens from realties
-DROP TABLE IF EXISTS expulsions;
+DROP TABLE IF EXISTS expulsions CASCADE;
 
 -- births of dreams by brains
-DROP TABLE IF EXISTS births;
+DROP TABLE IF EXISTS births CASCADE;
 
 -- phenomena of dreams by Looking at the Moon
-DROP TABLE IF EXISTS phenomena;
+DROP TABLE IF EXISTS phenomena CASCADE;
--- insertions.sql
+++ insertions.sql
@@ -17,7 +17,7 @@ INSERT INTO realties ( id ) VALUES ( DEFAULT );
 INSERT INTO brains ( id ) VALUES ( DEFAULT );
 
 -- looking_at_the_moon table
-INSERT INTO looking_at_the_moon ( id, fk_brain_id, fk_owner_id ) VALUES ( DEFAULT, ( SELECT id FROM brains WHERE id = 0 ), ( SELECT id FROM owners WHERE id = 0 ) );
+INSERT INTO looking_at_the_moon ( id, fk_brain_id, fk_owner_id ) VALUES ( DEFAULT, ( SELECT id FROM brains WHERE id = 1 ), ( SELECT id FROM owners WHERE id = 1 ) );
 
 -- states for impulses table
 INSERT INTO states ( name )
@@ -28,13 +28,13 @@ INSERT INTO states ( name )
     ( 'совершенно пробужден' );
 
 -- impulses to characterize looking_at_the_moon table
-INSERT INTO impulses ( id, fk_looking_at_the_moon_id, fk_state_name ) VALUES ( ( SELECT id FROM looking_at_the_moon WHERE id = 0 ), ( SELECT name FROM states WHERE name LIKE 'совершенно усыплен' ) );
+INSERT INTO impulses ( id, fk_looking_at_the_moon_id, fk_state_name ) VALUES ( DEFAULT, ( SELECT id FROM looking_at_the_moon WHERE id = 1 ), ( SELECT name FROM states WHERE name LIKE 'совершенно усыплен' ) );
 
 -- properties for impulses table
 INSERT INTO properties ( fk_impulse_id, name )
   VALUES
-    ( ( SELECT id FROM impulses WHERE id = 0 ), 'привычный'   ),
-    ( ( SELECT id FROM impulses WHERE id = 0 ), 'интуитивный' );
+    ( ( SELECT id FROM impulses WHERE id = 1 ), 'привычный'   ),
+    ( ( SELECT id FROM impulses WHERE id = 1 ), 'интуитивный' );
 
 -- materials for monoliths
 INSERT INTO materials ( name )
@@ -46,20 +46,20 @@ INSERT INTO materials ( name )
 INSERT INTO monoliths ( id, fk_material_name ) VALUES ( DEFAULT, ( SELECT name FROM materials WHERE name LIKE 'кристаллический' ) );
 
 -- dreams of monoliths and brains table
-INSERT INTO dreams ( id, fk_monolith_id ) VALUES ( DEFAULT, ( SELECT id FROM monoliths WHERE id = 0 ) );
+INSERT INTO dreams ( id, fk_monolith_id ) VALUES ( DEFAULT, ( SELECT id FROM monoliths WHERE id = 1 ) );
 
 -- images of dreams table
 INSERT INTO images ( id, is_real, fk_dream_id )
   VALUES
-    ( DEFAULT, true, ( SELECT id FROM dreams WHERE id = 0  ) ),
-    ( DEFAULT, true, ( SELECT id FROM dreams WHERE id = 0 ) );
+    ( DEFAULT, true, ( SELECT id FROM dreams WHERE id = 1  ) ),
+    ( DEFAULT, true, ( SELECT id FROM dreams WHERE id = 1 ) );
 
 -- aliens of realties table
 -- not the Ridley Scott aliens uWu
 INSERT INTO aliens ( id, fk_realty_id )
   VALUES
-    ( DEFAULT, ( SELECT id FROM realties WHERE id = 0 ) ),
-    ( DEFAULT, ( SELECT id FROM realties WHERE id = 0 ) );
+    ( DEFAULT, ( SELECT id FROM realties WHERE id = 1 ) ),
+    ( DEFAULT, ( SELECT id FROM realties WHERE id = 1 ) );
 
 
 -- expulsions of aliens from realties
@@ -72,11 +72,11 @@ INSERT INTO aliens ( id, fk_realty_id )
 
 INSERT INTO expulsions ( fk_impulse_id, fk_realty_id, is_success )
   VALUES (
-    ( SELECT id FROM impulses WHERE id = 0 ),
-    ( SELECT id FROM realties WHERE id = 0 ),
-    CASE SELECT fk_state_name FROM impulses WHERE id = ( SELECT id FROM impulses WHERE id = 0 )
-      WHEN LIKE '%пробужден'
-      THEN true 
+    ( SELECT id FROM impulses WHERE id = 1 ),
+    ( SELECT id FROM realties WHERE id = 1 ),
+    CASE ( SELECT fk_state_name FROM impulses WHERE id = ( SELECT id FROM impulses WHERE id = 1 ) )
+      WHEN 'пробужден' THEN true 
+      WHEN 'совершенно пробужден' THEN true
       ELSE false 
     END
 );
